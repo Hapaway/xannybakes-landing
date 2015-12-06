7ae8c48728f1f863fbc1d60f4a6f5c2da18459ef
Adrian Unger <dev@staydecent.ca>
2015-12-05 22:00:34 -0800

    Added contact form endpoint.

--- index.php
+++ index.php
@@ -8,6 +8,31 @@ Gum::get('/', function() {
   echo tpl('xannybakes');
 });
 
+Gum::post('/contact', function() {
+  $mandrill = new Mandrill(''); // @TODO: get from env var
+
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $body = htmlspecialchars($_POST['body']);
+  
+  $message = array(
+    'text' => $body,
+    'subject' => 'Someone contacted you via your website!',
+    'from_email' => $email,
+    'from_name' => $name,
+    'to' => array(
+      array(
+        'email' => 'dev@staydecent.ca',
+        'name' => 'Adrian Unger',
+        'type' => 'to'
+      )
+    )
+  );
+
+  $result = $mandrill->messages->send($message);
+  print_r($result);
+});
+
 // handle 404
 if (Gum::not_found()) {
   header('HTTP/1.0 404 Not Found');
